b8e620aab41e52c2b042b206068e6eb143cd1fc4
AlexPaturov <alexey.paturov@gmail.com>
2024-05-14 12:57:40 +0300

    Форматирование.

--- SerialToIpGUI/Resource1.cs
+++ SerialToIpGUI/Resource1.cs
@@ -1,10 +1,4 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: SerialToIpGUI.Resource1
-// Assembly: SerialToIPGUI, Version=1.9.5866.19643, Culture=neutral, PublicKeyToken=null
-// MVID: B1D67A1F-B8BA-49F3-B87A-0CFB4BE0BA84
-// Assembly location: C:\Users\Alex\Downloads\SerialToIPGUI_v1.9_2016-01-23\SerialToIPGUI.exe
-
-using System.CodeDom.Compiler;
+﻿using System.CodeDom.Compiler;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Drawing;
